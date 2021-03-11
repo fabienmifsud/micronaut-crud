4108ad848718f6a11cfddbd8c48f116bc41700d5
fmifsud <f.mifsud@traxens.com>
2021-03-11 11:11:44 +0100

    Update documentation

--- src/test/java/com/fmi/MicronautCrudTest.java
+++ src/test/java/com/fmi/MicronautCrudTest.java
@@ -1,14 +1,16 @@
 package com.fmi;
 
+import com.fmi.micronaut.Application;
 import io.micronaut.runtime.EmbeddedApplication;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import spock.lang.Specification;
 
 import javax.inject.Inject;
 
-@MicronautTest
-class MicronautCrudTest {
+@MicronautTest(application = Application.class)
+class MicronautCrudTest extends Specification {
 
     @Inject
     EmbeddedApplication<?> application;
@@ -17,5 +19,4 @@ class MicronautCrudTest {
     void testItWorks() {
         Assertions.assertTrue(application.isRunning());
     }
-
 }
